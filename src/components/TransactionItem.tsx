0fbfc3453a2e32628570f6a783fc76c9182d49ad
Sergey Devlyashov <devlyashovpro@gmail.com>
2026-08-18 10:48:21 +0500

    Change style in CSS

--- src/components/TransactionItem.tsx
+++ src/components/TransactionItem.tsx
@@ -2,6 +2,7 @@ import { memo } from "react"
 import { Link} from 'react-router-dom'
 import { useTransactionsContext } from "../context/useTransactionsContext"
 import type { Transaction } from "../types"
+import formatDate from "../utils/formatDate"
 
 type TransactionItemProps = {
     transaction: Transaction;
@@ -10,29 +11,45 @@ type TransactionItemProps = {
 function TransactionItem({transaction}: TransactionItemProps) {
     const {deleteTransaction, categories} = useTransactionsContext()
 
-    const {id, category, text, amount} = transaction
+    const {id, category, text, amount, date} = transaction
     
     const categoryObj = categories.find((obj)=>obj.key === category)
     
     const categoryName = categoryObj?.name ?? 'Неизвестная категория'
       
     return (
-        <li className={amount < 0 ? "minus" : "plus"}>
-      <div className="categorytext">
-        <p className="category" >{categoryName}</p>
-        <h4 className="transaction-title">{text}</h4>
-      </div>
+  <li className={amount < 0 ? "minus" : "plus"}>
+    <div className="categorytext">
+      <p className="category">{categoryName}</p>
+      <h4 className="transaction-title">{text}</h4>
+    </div>
 
-      <div className="transaction_amount">
-      <Link to={`/transaction/${id}/edit`} className='buttonHomePage edit-btn'><img className="edit-logo" src='/edit-logo.png'></img></Link>
-        <span>
+    <div className="transaction_amount">
+      <Link
+        to={`/transaction/${id}/edit`}
+        className="buttonHomePage edit-btn"
+      >
+        <img className="edit-logo" src="/edit-logo.png" alt="Редактировать" />
+      </Link>
+
+      <div className="transaction-main-info">
+        <span className="transactionAmountValue">
           {amount > 0 ? "+" : ""}
           {amount}
         </span>
-        <button className="delete-btn" onClick={() => deleteTransaction(id)}>х</button>
+
+        <p className="transactionDate">{formatDate(date)}</p>
       </div>
-    </li>
-    )
+
+      <button
+        className="delete-btn"
+        onClick={() => deleteTransaction(id)}
+      >
+        х
+      </button>
+    </div>
+  </li>
+)
 }
 
 export default memo(TransactionItem)
